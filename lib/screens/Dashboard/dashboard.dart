f13bee91a4d5dac1f2e1125aedefd0b3f5940740
Vinula Senarathne <vinulapc@gmail.com>
2024-04-19 22:34:43 +0530

    feat: added flash deal timer

--- lib/screens/Dashboard/dashboard.dart
+++ lib/screens/Dashboard/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:slide_countdown/slide_countdown.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -108,18 +109,73 @@ class _DashboardState extends State<Dashboard> {
           Container(
             margin:
                 EdgeInsets.symmetric(horizontal: deviceData.size.width * 0.05),
-            height: 200,
+            height: 85,
             child: ListView.separated(
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
                   return GestureDetector(
-                    child: Text("Shoes"),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: const BoxDecoration(
+                            color: Color(0xffF5F5FF),
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage("assets/images/shoe.png"),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Shoes",
+                          style:
+                              GoogleFonts.poppins(fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
                   );
                 },
                 separatorBuilder: (contetx, _) => const SizedBox(
-                      width: 10,
+                      width: 25,
                     ),
-                itemCount: 5),
+                itemCount: 8),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // * Flash sale
+          Container(
+            margin:
+                EdgeInsets.symmetric(horizontal: deviceData.size.width * 0.05),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Flash Sale",
+                  style: GoogleFonts.poppins(
+                      fontSize: 18, fontWeight: FontWeight.w700),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Closing in:",
+                      style: GoogleFonts.poppins(),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    SlideCountdownSeparated(
+                      separator: " ",
+                      duration: const Duration(hours: 12),
+                    )
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
