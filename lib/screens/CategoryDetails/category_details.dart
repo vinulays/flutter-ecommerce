bc6e8e97d0cb8256dcbbf9e23783c3b3081eff05
Vinula Senarathne <vinulapc@gmail.com>
2024-04-28 08:14:14 +0530

    feat: order details screen

--- lib/screens/CategoryDetails/category_details.dart
+++ lib/screens/CategoryDetails/category_details.dart
@@ -1,7 +1,6 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/screens/CategoryDetails/bloc/category_details_bloc.dart';
