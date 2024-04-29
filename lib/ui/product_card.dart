436b77c9fb5f1b475f479625a8ce99faa382914a
Vinula Senarathne <vinulapc@gmail.com>
2024-04-29 11:45:57 +0530

    fix: search margin

--- lib/ui/product_card.dart
+++ lib/ui/product_card.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/screens/ProductDetails/bloc/product_details_bloc.dart';
